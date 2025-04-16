cc6aa0a81dfc1624206145badb4ea13d692ea683
karthikeyan <55027856+karthikeyanbaluchamy@users.noreply.github.com>
2025-04-16 12:44:36 -0400

    Add files via upload

---
+++ .github/actions/deploy-javaScript/main.js
@@ -0,0 +1,12 @@
+
+const core= require('@actions/core')
+const github= require('@actions/github')
+const exec= require('@actions/exec')
+
+function run(){
+	
+	core.notice('hello from custom java script') 
+	
+}
+
+run();
